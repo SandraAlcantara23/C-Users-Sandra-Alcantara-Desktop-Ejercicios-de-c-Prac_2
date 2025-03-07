7b9eb0c45294d5363f177dd04dce23b9f85c2f7c
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2025-03-06 20:10:08 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const ProductRoutes = require("./routers/products.routes");
+const { AppDataSource } = require("./data/source");
+const express = require("express")
+const cors = require("cors");
+const PORT = 3000;
+
+
+const app = express();
+
+//Middlewares
+
+app.use(express.json());
+app.use(cors());
+
+
+//Rutas
+app.use("/api/v1",ProductRoutes)
+
+AppDataSource.initialize().then(
+ ()=>{
+     console.log("Conectado a la base de datos");
+     app.listen(PORT, ()=> console.log("servidor corriendo en http://localhost:"
+     +PORT));
+   }
+).catch((err)=>{
+    console.log("Error al conectar base de datos");
+    console.error(err);
+})
+
+
+
+    res.status(204)
