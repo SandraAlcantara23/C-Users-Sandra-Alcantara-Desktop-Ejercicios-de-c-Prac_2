1c8b6c6b99b4ca29d925d848b818f63bc5aa73d3
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2025-04-03 19:11:39 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-const ProductRoutes = require("./routers/products.routes");
+const ProductRoutes = require("./routes/products.routers"); 
 const { AppDataSource } = require("./data/source");
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
+
 const PORT = 3000;
-
-
 const app = express();
 
-//Middlewares
-
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
+// Rutas
+app.use("/api/v1", ProductRoutes);
 
-//Rutas
-app.use("/api/v1",ProductRoutes)
-
-AppDataSource.initialize().then(
- ()=>{
-     console.log("Conectado a la base de datos");
-     app.listen(PORT, ()=> console.log("servidor corriendo en http://localhost:"
-     +PORT));
-   }
-).catch((err)=>{
-    console.log("Error al conectar base de datos");
-    console.error(err);
-})
-
-
-
-    res.status(204)
+// Conectar a la base de datos y levantar el servidor
+AppDataSource.initialize().then(() => {
+    console.log("Conectado a la base de datos");
+    app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+}).catch((err) => {
+    console.error("Error al conectar a la base de datos", err);
+});
